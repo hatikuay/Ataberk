606d361a997869de6811581ad48842b294443ae8
hatikuay <ercan.erkalkan@marmara.edu.tr>
2025-03-24 21:31:45 +0300

    commit

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -1,11 +1,11 @@
-package com.example.demo;
+package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-public class HelloController {
+public class BookController {
     @GetMapping("/")
     public String hello() {
         return "Merhaba Spring Boot!";
